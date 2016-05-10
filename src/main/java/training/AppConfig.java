c9e3674d02f83f200e0c3205a3132aa3a6779af5
Oksana Eryomenko <Oksana_Eryomenko@epam.com>
2016-05-10 13:21:53 +0200

    reformating code

---
+++ src/main/java/training/AppConfig.java
@@ -0,0 +1,83 @@
+package training;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import training.model.Issue;
+import training.model.Project;
+import training.model.User;
+import training.service.UserService;
+import training.service.UserServiceImpl;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+/**
+ * Created by Oksana_Eryomenko on 5/9/2016.
+ */
+
+@Configuration
+@ComponentScan("training")
+@EnableTransactionManagement
+public class AppConfig {
+
+    @Bean
+    public User user() {
+        return new User();
+    }
+
+    @Bean
+    public Project project() {
+        return new Project();
+    }
+
+    @Bean
+    public Issue issue() {
+        return new Issue();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory
+            (DataSource dataSource, JpaVendorAdapter jpaVendeorAdapter) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactory = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactory.setDataSource(dataSource);
+        entityManagerFactory.setJpaVendorAdapter(jpaVendeorAdapter);
+        entityManagerFactory.setPackagesToScan("training");
+        return entityManagerFactory;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter adapter = new HibernateJpaVendorAdapter();
+        adapter.setShowSql(true);
+        adapter.setGenerateDdl(false);
+        adapter.setDatabasePlatform("org.hibernate.dialect.MySQLDialect");
+
+        return adapter;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource ds = new DriverManagerDataSource();
+        ds.setDriverClassName("com.mysql.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost:3306/mydb");
+        ds.setUsername("root");
+        ds.setPassword("nihao23");
+
+        return ds;
+    }
+
+
+}
